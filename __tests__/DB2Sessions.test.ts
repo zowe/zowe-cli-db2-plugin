6969f2a0384208dbc68665bff334c7db8acc4595
rn668075 <rauan.nurbatyrov@broadcom.com>
2019-02-07 17:05:36 +0100

    add test of DB2Sessions.class
    Signed-off-by: rn668075 <rauan.nurbatyrov@broadcom.com>

---
+++ __tests__/DB2Sessions.test.ts
@@ -0,0 +1,53 @@
+/*
+* This program and the accompanying materials are made available under the terms of the *
+* Eclipse Public License v2.0 which accompanies this distribution, and is available at *
+* https://www.eclipse.org/legal/epl-v20.html                                      *
+*                                                                                 *
+* SPDX-License-Identifier: EPL-2.0                                                *
+*                                                                                 *
+* Copyright Contributors to the Zowe Project.                                     *
+*                                                                                 *
+*/
+
+import { ICommandArguments, IProfile } from "@brightside/imperative";
+import { DB2Session } from "../src/cli/DB2Sessions";
+import { SESSION } from "./__src__/Db2TestConstants";
+import { IDB2Session } from "../src/rest/session/doc/IDB2Session";
+
+describe("createDB2Session()", () => {
+    let args: ICommandArguments = {$0: "", _: [], ...SESSION};
+    let profile: IProfile = { ...SESSION };
+    it("should create a session from cmd arguments if profile is undefined", () => {
+        let session = DB2Session.createDB2Session(args, profile);
+        session = session.ISession as IDB2Session;
+
+        expect(session.hostname).toEqual(SESSION.hostname);
+        expect(session.port).toEqual(SESSION.port);
+        expect(session.username).toEqual(SESSION.username);
+        expect(session.database).toEqual(SESSION.database);
+    });
+
+    it("should create a session from profile if cmd arguments are undefined", () => {
+        args = {$0: "", _: []};
+        let session = DB2Session.createDB2Session(args, profile);
+        session = session.ISession as IDB2Session;
+
+        expect(session.hostname).toEqual(SESSION.hostname);
+        expect(session.port).toEqual(SESSION.port);
+        expect(session.username).toEqual(SESSION.username);
+        expect(session.database).toEqual(SESSION.database);
+    });
+    it("should create a session and overrid profile with cmd arguments", () => {
+        args = {$0: "", _: [], ...SESSION};
+        profile = {...profile, hostname: "some-test-host", port: 1111111};
+        let session = DB2Session.createDB2Session(args, profile);
+        session = session.ISession as IDB2Session;
+
+        expect(session.hostname).not.toEqual("some-test-host");
+        expect(session.port).not.toEqual(1111111);
+        expect(session.hostname).toEqual(SESSION.hostname);
+        expect(session.port).toEqual(SESSION.port);
+        expect(session.username).toEqual(SESSION.username);
+        expect(session.database).toEqual(SESSION.database);
+    });
+});
