b3cfd34a9c2c5b889c56a89ce58cc21bcb604941
Brandon Jenkins <36675406+BrandonJenkins14@users.noreply.github.com>
2020-12-03 16:52:51 -0500

    Add an example for passing outputs from an sp
    Signed-off-by: Brandon Jenkins <36675406+BrandonJenkins14@users.noreply.github.com>

--- src/cli/call/procedure/Procedure.definition.ts
+++ src/cli/call/procedure/Procedure.definition.ts
@@ -35,7 +35,7 @@ export const ProcedureDefinition: ICommandDefinition = {
             name: "parameters",
             aliases: ["p"],
             type: "array",
-            description: "Values to bind to the stored procedure parameters",
+            description: "Values to bind to the stored procedure parameters.",
             required: false,
         },
     ],
@@ -47,6 +47,10 @@ export const ProcedureDefinition: ICommandDefinition = {
         {
             description: "Call a stored procedure and pass values for parameter indicators",
             options: "\"DEMO.SP2(?, ?)\" --parameters \"Hello\" \"world!\"",
+        },
+        {
+            description: "Call a stored procedure and pass values for two output parameters. The first output requires a 2-character buffer. The second output is a message that will be truncated to the length of the placeholder.",
+            options: "\"DEMO.SP3(NULL, ?, ?)\" --parameters "00" "message_placeholder_message_placeholder"",
         },
     ],
 };
