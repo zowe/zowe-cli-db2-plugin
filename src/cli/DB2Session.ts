c7585357e923b95420a85366be9cc1200729afd9
Andrew W. Harn <andrew.harn@broadcom.com>
2022-04-05 10:43:35 -0400

    Update typedoc and make parameter clear in typedoc
    Signed-off-by: Andrew W. Harn <andrew.harn@broadcom.com>

--- src/cli/DB2Session.ts
+++ src/cli/DB2Session.ts
@@ -124,9 +124,10 @@ export class DB2Session {
      * @static
      * @param {IProfile} args - The arguments specified by the user
      * @param {boolean} doPrompting - Whether to prompt for missing arguments (defaults to true)
+     * @param {IHandlerParameters} handlerParams - The command parameters object for Daemon mode prompting
      * @returns {Session} - A session for usage in the CMCI REST Client
      */
-    public static async createSessCfgFromArgs(args: ICommandArguments, doPrompting = true, handler?: IHandlerParameters): Promise<Session> {
+    public static async createSessCfgFromArgs(args: ICommandArguments, doPrompting = true, handlerParams?: IHandlerParameters): Promise<Session> {
         const sessCfg: IDB2Session = {
             hostname: args.host,
             port: args.port,
@@ -136,7 +137,7 @@ export class DB2Session {
             sslFile: args.sslFile,
         };
 
-        const sessCfgWithCreds = await ConnectionPropsForSessCfg.addPropsOrPrompt<IDB2Session>(sessCfg, args, {doPrompting, parms: handler});
+        const sessCfgWithCreds = await ConnectionPropsForSessCfg.addPropsOrPrompt<IDB2Session>(sessCfg, args, {doPrompting, parms: handlerParams});
         return new Session(sessCfgWithCreds);
     }
 
